8001c91ff90c1e7afb38c44af3a7ef316e0e8e6f
Anastasia Murashko <nivanis@yandex.ru>
2020-07-17 18:50:29 +0300

    add fix

--- src/test/java/util/MethodWait.java
+++ src/test/java/util/MethodWait.java
@@ -8,14 +8,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MethodWait {
     public static void waitPresenceOfElementLocated(WebDriver driver, By by) {
-        new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(by));
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
     public static void waitElementToBeClickable(WebDriver driver, WebElement element) {
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(element));
+        new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static void waitElementToBeClickable(WebDriver driver, By by) {
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(by));
+        new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(by));
     }
 }
